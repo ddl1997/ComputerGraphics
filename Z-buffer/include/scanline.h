875a6bed69f0538da9abc09ef9c00e0b7053c7ff
ddl <1055048157@qq.com>
2021-12-23 20:26:39 +0800

    继续实现

--- Z-buffer/include/scanline.h
+++ Z-buffer/include/scanline.h
@@ -6,6 +6,7 @@
 #include <utils/buffer.h>
 #include <Eigen/Dense>
 #include <vector>
+#include <algorithm>
 
 namespace scanline {
 
@@ -21,6 +22,11 @@ namespace scanline {
         float dx;
         bool isHorizontal;
     };
+
+    bool edgeCmp(edge a, edge b)
+    {
+        return (a.ymin < b.ymin) || (a.ymin == b.ymin && a.dx < b.dx);
+    }
 
     struct triFace {
         edge edges[3];
@@ -79,6 +85,9 @@ namespace scanline {
                     {
                         tri.vertices[i] = matrix * Eigen::Vector4f(vs[i].Position, 1.0);
                         tri.vertices[i] /= tri.vertices[i].w();
+                        // 转换到视口坐标系 [(0,0) ~ (width,height)]
+                        tri.vertices[i].x() = 0.5 * IMAGE_WIDTH * (tri.vertices[i].x() + 1);
+                        tri.vertices[i].y() = 0.5 * IMAGE_HEIGHT * (tri.vertices[i].y() + 1);
                     }
                         
                     float xmin = getMinOfThree(tri.vertices[0].x(), tri.vertices[1].x(), tri.vertices[2].x()),
@@ -87,20 +96,24 @@ namespace scanline {
                         ymax = getMaxOfThree(tri.vertices[0].y(), tri.vertices[1].y(), tri.vertices[2].y());
                     tri.lu = Eigen::Vector2f(xmin, ymax);
                     tri.rb = Eigen::Vector2f(xmax, ymin);
+                    std::vector<edge> temp;
                     for (int i = 0; i < 3; i++)
                     {
                         auto v1 = tri.vertices[i];
                         auto v2 = tri.vertices[(i + 1) % 3];
                         float x1 = v1.x(), y1 = v1.y(), x2 = v2.x(), y2 = v2.y();
+                        edge e;
                         if (y1 == y2)
-                            tri.edges[i].isHorizontal = true;
+                            e.isHorizontal = true;
                         else
                         {
-                            tri.edges[i].isHorizontal = false;
-                            tri.edges[i].ymax = y1 > y2 ? y1 : y2;
-                            tri.edges[i].ymin = y1 < y2 ? y1 : y2;
-                            tri.edges[i].dx = (x1 - x2) / (y1 - y2);
+                            e.isHorizontal = false;
+                            e.ymax = y1 > y2 ? y1 : y2;
+                            e.ymin = y1 < y2 ? y1 : y2;
+                            e.x = y1 < y2 ? x1 : x2;
+                            e.dx = (x1 - x2) / (y1 - y2);
                         }
+                        temp.push_back(e);
                     }
                     tris.push_back(tri);
                 }
@@ -118,6 +131,24 @@ namespace scanline {
 
         for (auto f : faces)
         {
+            float start = std::floor(f.rb.y()) - 0.5;
+            float end = std::ceil(f.lu.y()) + 0.5;
+
+            for (; start <= end; start += 1)
+            {
+                Eigen::Vector2f intersects[3] = { Eigen::Vector2f(-1, -1), Eigen::Vector2f(-1, -1), Eigen::Vector2f(-1, -1) };
+                for (int i = 0; i < 3; i++)
+                {
+                    auto e = f.edges[i];
+                    if (e.isHorizontal)
+                        continue;
+                    else if (e.ymin < start)
+                    {
+                        intersects.push_back(Eigen::Vector2f(e.x + e.dx * (start)));
+                    }
+
+                }
+            }
 
         }
 
